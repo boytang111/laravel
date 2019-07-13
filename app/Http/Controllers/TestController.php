a5002f4bbf140a56ac8a456f9a7dd2d5054c7c2b
tony <47001279@qq.com>
2019-07-13 20:30:21 +0800

    test

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+class TestController extends Controller
+{
+    //
+    function index(){
+        return view('Test', ['data' => 111]);
+        DB::beginTransaction();
+
+        try{  //中间逻辑代码
+            $res = DB::table('user')->insertGetId(
+                ['name' => 'tony', 'head_img' => 'aaaaaa']
+            );
+            DB::commit();
+            return view('Test', ['data' => 2222]);
+        }catch (\Exception $e) {
+
+            //接收异常处理并回滚
+
+            DB::rollBack();
+            return view('Test', ['data' => 111]);
+        }
+
+
+                // var_dump($res);exit;
+
+
+
+    }
+}
